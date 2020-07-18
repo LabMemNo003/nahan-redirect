13ced1e3dd4935216ad2ef9701c4c133feb41b3c
LabMemNo003 <LabMemNo003@gmail.com>
2020-07-18 22:13:55 +0800

    Fix a typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = Redirect;
 
 function Redirect(path, status = 302) {
 
-    if (![301, 302].includes(res_status_code))
+    if (![301, 302].includes(status))
         throw new Error('Invalid response status code.');
 
     return async (ctx, next) => {
